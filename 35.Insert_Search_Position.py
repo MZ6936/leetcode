bf1c31fea10d7d96e7d41fe6c8c3e1a48c7917ab
MZ6936 <mirzakarim.in@gmail.com>
2026-02-12 23:43:52 +0530

    Update 35.Insert_Search_Position.py
    Shortened the code

--- 35.Insert_Search_Position.py
+++ 35.Insert_Search_Position.py
@@ -1,17 +1,9 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         for i,num in enumerate(nums):
-            if num == target:
+            if(target <= nums[i]):
                 return i
                 break
-        for i,num in enumerate(nums):
-            if(target < nums[i]):
-                return i
-                break
-            elif(i == len(nums)-1):
+            elif(i == len(nums)-1 or nums[i] < target < nums[i+1]):
                 return i+1
                 break
-            elif(nums[i] < target < nums[i+1]):
-                return i+1
-                break
-        
