011232b7a311ad4861b82c0ddea0915040be7fc7
MZ6936 <mirzakarim.in@gmail.com>
2026-02-12 23:36:10 +0530

    Update 35.Insert_Search_Position.py

--- 35.Insert_Search_Position.py
+++ 35.Insert_Search_Position.py
@@ -7,6 +7,7 @@ class Solution:
         for i,num in enumerate(nums):
             if(target < nums[i]):
                 return i
+                break
             elif(i == len(nums)-1):
                 return i+1
                 break
